d90c393574edea2326782652c7e8f003652e45c8
GeorgeBark <GeorgeBark@users.noreply.github.com>
2023-11-06 23:21:19 +0100

    adding some global variable to flow

--- flows/test_flow/test_flow.py
+++ flows/test_flow/test_flow.py
@@ -1,7 +1,8 @@
 from prefect import flow, get_run_logger
 
+TESTING = 'hello'
 
 @flow
 def my_docker_flow():
     logger = get_run_logger()
-    logger.info("Hello from Docker!")
+    logger.info(f"Hello from Docker!, the variable is {TESTING}")
